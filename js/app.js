de6cc66a78af8797b35d29d81724ed4ed79fe212
Kelly Hwu <kelhwu@uw.edu>
2018-06-03 21:52:57 -0700

    Added files

---
+++ js/app.js
@@ -0,0 +1,419 @@
+const ICECREAM = [
+    {
+        "name": "Molly Moon's Homemade Ice Cream",
+        "neighborhood": "Capitol Hill",
+        "type": "Ice Cream",
+        "location": "917 E Pine St",
+        "website": "mollymoon.com",
+        "latitude": "47.615137",
+        "longitude": "-122.319836"
+    },
+    {
+        "name": "Molly Moon's Homemade Ice Cream",
+        "neighborhood": "Queen Anne",
+        "type": "Ice Cream",
+        "location": "321 W Galer St",
+        "website": "mollymoon.com",
+        "latitude": "47.632181",
+        "longitude": "-122.361466"
+    },
+    {
+        "name": "Molly Moon's Homemade Ice Cream",
+        "neighborhood": "Wallingford",
+        "type": "Ice Cream",
+        "location": "1622 1/2 N 45th St",
+        "website": "mollymoon.com",
+        "latitude": "47.661531",
+        "longitude": "-122.338166"
+    },
+    {
+        "name": "Molly Moon's Homemade Ice Cream",
+        "neighborhood": "Madrona",
+        "type": "Ice Cream",
+        "location": "1408 34th Ave",
+        "website": "mollymoon.com",
+        "latitude": "47.61322550000001",
+        "longitude": "-122.28905170000002"
+    },
+    {
+        "name": "Molly Moon's Homemade Ice Cream",
+        "neighborhood": "19th/Mercer",
+        "type": "Ice Cream",
+        "location": "522 19th Ave. E",
+        "website": "mollymoon.com",
+        "latitude": "47.6239588",
+        "longitude": "-122.30699930000003"
+    },
+    {
+        "name": "Molly Moon's Homemade Ice Cream",
+        "neighborhood": "Columbia City",
+        "type": "Ice Cream",
+        "location": "4822 Rainier Ave S",
+        "website": "mollymoon.com",
+        "latitude": "47.5588159",
+        "longitude": "-122.2853412"
+    },
+    {
+        "name": "Molly Moon's Homemade Ice Cream",
+        "neighborhood": "UVillage",
+        "type": "Ice Cream",
+        "location": "2615 NE 46th St",
+        "website": "mollymoon.com",
+        "latitude": "47.661610",
+        "longitude": "-122.299610"
+    },
+    {
+        "name": "Salt & Straw",
+        "type": "Ice Cream",
+        "location": "2615 NE 46th St",
+        "website": "saltandstraw.com",
+        "latitude": "47.6683606",
+        "longitude": "-122.38530609999998"
+    },
+    {
+        "name": "Salt & Straw",
+        "type": "Ice Cream",
+        "location": "714 E Pike St",
+        "website": "saltandstraw.com",
+        "latitude": "47.6144958",
+        "longitude": "-122.3227215"
+    },
+    {
+        "name": "Shug's Soda Foundation + Ice Cream",
+        "type": "Ice Cream",
+        "location": "1525 1st Ave",
+        "website": "shugssodafountain.com",
+        "latitude": "47.6095422",
+        "longitude": "-122.3409297"
+    },
+    {
+        "name": "Sweet Alchemy",
+        "type": "Ice Cream",
+        "location": "4301 University Way NE",
+        "website": "sweetalchemyicecreamery.com",
+        "latitude": "47.6599166",
+        "longitude": "-122.3133762"
+    },
+    {
+        "name": "Kurt Farm Shop",
+        "type": "Ice Cream",
+        "location": "1424 11th Ave",
+        "website": "kurtwoodfarms.com",
+        "latitude": "47.613715",
+        "longitude": "-122.317726"
+    },
+    {
+        "name": "Parfait Ice Cream",
+        "type": "Ice Cream",
+        "location": "2034 NW 56th St",
+        "website": "parfait-icecream.com",
+        "latitude": "47.6696934",
+        "longitude": "-122.38373869999998"
+    },
+    {
+        "name": "Full Tilt Ice Cream",
+        "type": "Ice Cream",
+        "location": "328 15th Ave E",
+        "website": "fulltilticecream.com",
+        "latitude": "47.6214755",
+        "longitude": "-122.31248549999998"
+    },
+    {
+        "name": "Full Tilt Ice Cream",
+        "type": "Ice Cream",
+        "location": "9629 16th Ave SW",
+        "website": "fulltilticecream.com",
+        "latitude": "47.5164279",
+        "longitude": "-122.3554866"
+    },
+    {
+        "name": "Full Tilt Ice Cream",
+        "type": "Ice Cream",
+        "location": "4759 Brooklyn Ave NE",
+        "website": "fulltilticecream.com",
+        "latitude": "47.6647605",
+        "longitude": "-122.31448649999999"
+    },
+    {
+        "name": "Full Tilt Ice Cream",
+        "type": "Ice Cream",
+        "location": "5041 Rainier Ave S",
+        "website": "fulltilticecream.com",
+        "latitude": "47.555982",
+        "longitude": "-122.28421700000001"
+    },
+    {
+        "name": "Full Tilt Ice Cream",
+        "type": "Ice Cream",
+        "location": "5453 Leary Ave NW",
+        "website": "fulltilticecream.com",
+        "latitude": "47.6678817",
+        "longitude": "-122.3839481"
+    },
+    {
+        "name": "Bluebird Ice Cream",
+        "type": "Ice Cream",
+        "location": "7400 Greenwood Ave N",
+        "website": "www.facebook.com/bluebirdicecrm",
+        "latitude": "47.6826614",
+        "longitude": "-122.35507280000002"
+    },
+    {
+        "name": "Bluebird Ice Cream",
+        "type": "Ice Cream",
+        "location": "3515 Fremont Ave N",
+        "website": "www.facebook.com/bluebirdicecrm",
+        "latitude": "47.651144",
+        "longitude": "-122.35022500000002"
+    },
+    {
+        "name": "Central District Ice Cream Company",
+        "type": "Ice Cream",
+        "location": "2016 E Union St",
+        "website": "cdicecream.com",
+        "latitude": "47.6130923",
+        "longitude": "-122.30546449999997"
+    },
+    {
+        "name": "Scoop Du Jour Ice Creamery",
+        "type": "Ice Cream",
+        "location": "4029 E Madison St",
+        "website": "www.yelp.com/biz/scoop-du-jour-ice-creamery-seattle",
+        "latitude": "47.634002",
+        "longitude": "-122.28018800000001"
+    },
+
+
+
+
+    {
+        "name": "Gelatiamo",
+        "type": "Gelato",
+        "location": "1400 3rd Ave",
+        "website": "gelatiamo.com",
+        "latitude": "47.60897910000001",
+        "longitude": "-122.33680329999999"
+    },
+    {
+        "name": "Fainting Goat",
+        "type": "Gelato",
+        "location": "1903 N 45th St",
+        "website": "faintinggoatseattle.com",
+        "latitude": "47.6612531",
+        "longitude": "-122.33477970000001"
+    },
+    {
+        "name": "Fainting Goat",
+        "type": "Gelato",
+        "location": "3415 Fremont Ave N",
+        "website": "faintinggoatseattle.com",
+        "latitude": "47.6501321",
+        "longitude": "-122.35007580000001"
+    },
+    {
+        "name": "Bottega Italiana",
+        "type": "Gelato",
+        "location": "1425 1st Ave",
+        "website": "bottegaitaliana.com",
+        "latitude": "47.60837",
+        "longitude": "-122.33983899999998"
+    },
+    {
+        "name": "D'Ambrosio Gelato",
+        "type": "Gelato",
+        "location": "5339 Ballard Ave NW",
+        "website": "dambrosiogelato.com",
+        "latitude": "47.6671692",
+        "longitude": "-122.38439879999999"
+    },
+    {
+        "name": "Nutty Squirrel Gelato",
+        "type": "Gelato",
+        "location": "7212 Greenwood Ave N",
+        "website": "nuttysquirrel.com",
+        "latitude": "47.6815135",
+        "longitude": "-122.3550568"
+    },
+    {
+        "name": "Nutty Squirrel Gelato",
+        "type": "Gelato",
+        "location": "2425 33rd Ave W",
+        "website": "nuttysquirrel.com",
+        "latitude": "47.6402385",
+        "longitude": "-122.39990419999998"
+    },
+
+
+
+    {
+        "name": "Zoeyogurt",
+        "type": "Froyo",
+        "location": "6900 East Green Lake Way N",
+        "website": "facebook.com/zoeyogurt",
+        "latitude": "47.6786584",
+        "longitude": "-122.32631229999998"
+    },
+    {
+        "name": "Menchie's",
+        "type": "Froyo",
+        "location": "507 Broadway E",
+        "website": "menchies.com",
+        "latitude": "47.6235247",
+        "longitude": "-122.32123690000003"
+    },
+    {
+        "name": "Menchie's",
+        "type": "Froyo",
+        "location": "2101 Queen Anne Ave N",
+        "website": "menchies.com",
+        "latitude": "47.6373069",
+        "longitude": "-122.35712519999998"
+    },
+    {
+        "name": "Menchie's",
+        "type": "Froyo",
+        "location": "2620 California Ave SW",
+        "website": "menchies.com",
+        "latitude": "47.5805689",
+        "longitude": "-122.38625279999997"
+    },
+    {
+        "name": "Menchie's",
+        "type": "Froyo",
+        "location": "4609 Village Ct NE",
+        "website": "menchies.com",
+        "latitude": "47.6618612",
+        "longitude": "-122.29937540000003"
+    },
+    {
+        "name": "Menchie's",
+        "type": "Froyo",
+        "location": "3411 W McGraw St",
+        "website": "menchies.com",
+        "latitude": "47.6394497",
+        "longitude": "-122.40169270000001"
+    },
+    {
+        "name": "Menchie's",
+        "type": "Froyo",
+        "location": "424 NE 71st St",
+        "website": "menchies.com",
+        "latitude": "47.6800881",
+        "longitude": "-122.3236157"
+    },
+    {
+        "name": "Old School Frozen Custard",
+        "type": "Frozen Custard",
+        "location": "1316 E Pike St",
+        "website": "yelp.com/biz/old-school-frozen-custard-seattle",
+        "latitude": "47.614275",
+        "longitude": "-122.31467299999997"
+    },
+    {
+        "name": "Frankie and Jo's",
+        "type": "Vegan",
+        "location": "1010 E Union St",
+        "website": "frankieandjos.com",
+        "latitude": "47.6130487",
+        "longitude": "-122.3191258"
+    },
+    {
+        "name": "Frankie and Jo's",
+        "type": "Vegan",
+        "location": "1411 NW 70th St",
+        "website": "frankieandjos.com",
+        "latitude": "47.6793619",
+        "longitude": "-122.37484130000001"
+    }
+];
+
+// Create the map object, set the view and zoom
+const mymap = L.map("mapid").setView([47.604311, -122.331734], 11.5);
+
+// Add the background tiles to the map
+L.tileLayer(
+    "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+    {
+        attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: "mapbox.streets",
+        accessToken:
+            "pk.eyJ1Ijoibmlja2RlbmFyZGlzIiwiYSI6ImNqaGRla2pjMjBvYXgzNm13Yzc3aGIwM3kifQ.G2Tr-B7ppCNdj6xuM0Qc5A"
+    }
+).addTo(mymap);
+
+// Parse the JSON response
+function parseAsJSON(response) {
+    // Return the JSON from the response
+    return response.json();
+}
+
+// What to do if there is an error
+function handleError(err) {
+    // Show the error to the user
+    console.error(err);
+    alert(err.message);
+}
+
+var markerGroup = L.layerGroup().addTo(mymap);
+
+// Render the map on screen
+function renderMap(data) {
+    // `data` is an array of objects
+    // Add each object to the map if `latitude` and `longitude` are available
+    // Use `bindPopup()` to add `name`, `type`, `address`, and `address` properties
+    data.forEach(function (response) {
+        var lat = response.latitude;
+        var long = response.longitude;
+
+        //console.log(lat);
+        //console.log(long);
+
+        if ((lat != null) || (long != null)) {
+            var poptext = response.name + '<br/>' + 'Location: ' + response.location + '<br/>' + 'Website: ' + response.website;
+            //var marker = L.marker([lat, long]).addTo(mymap).bindPopup(poptext);
+            var marker = L.marker([lat, long]).addTo(markerGroup).bindPopup(poptext);
+        } else {
+            console.log(lat + ' , ' + long + ' is invalid')
+        }
+
+    });
+
+}
+
+// Fetch the API datasource, parse JSON, render the map, and handle errors
+//renderMap(ICECREAM);
+
+
+// Get the input element from the HTML
+var searchInput = document.getElementById("animation-select");
+
+searchInput.addEventListener("input", function () {
+
+    console.log(searchInput.value);
+    // Filter the MOVIES array
+
+    //var ICECREAM_filtered = ICECREAM;
+
+    //remove(ICECREAM_filtered, "Ice Cream");
+
+
+
+    var ICECREAM_filtered = ICECREAM.filter(function (el) {
+        // Check to see if the movie title contains the search input field value
+        // Change both the movie title and the search input value to lowercase
+        return el['type'] === searchInput.value;
+    });
+
+    console.log(ICECREAM_filtered);
+
+    /*     mymap.eachLayer(function (layer) {
+            mymap.removeLayer(layer);
+        }); */
+    markerGroup.clearLayers();
+
+    // Render the table with the newly filtered movie list
+    renderMap(ICECREAM_filtered);
+
+});
